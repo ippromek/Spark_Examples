89916c25c9f98ec6b6cdaa989d1ba8bd6cfc7d77
ippromek <ippromek@gmail.com>
2017-02-25 07:02:39 +0000

    Add  Spark init

---
+++ src/main/scala/MainSpark.scala
@@ -0,0 +1,27 @@
+import java.lang.Long
+
+import org.apache.spark.sql.functions._
+
+final case class Person(firstName: String, lastName: String,
+                        country: String, age: Int)
+
+object Main extends InitSpark {
+  def main(args: Array[String]) = {
+    import spark.implicits._
+
+    val version = spark.version
+    println("SPARK VERSION = " + version)
+
+    val sumHundred: Long = spark.range(1, 101).reduce(_ + _)
+    println(f"Sum 1 to 100 = $sumHundred")
+
+    println("Reading from csv file: people-example.csv")
+    val persons = reader.csv("src/main/resources/people-example.csv").as[Person]
+    persons.show(2)
+    val averageAge = persons.agg(avg("age"))
+      .first.get(0).asInstanceOf[Double]
+    println(f"Average Age: $averageAge%.2f")
+
+
+  }
+}
